113c3b72cc3802c5f59743a1b3a66d1e9567082a
hidaris <zuocool@gmail.com>
2022-11-18 18:18:38 +0800

    remove projectile

--- init.el
+++ init.el
@@ -344,17 +344,19 @@ Start `ielm' if it's not already running."
 (use-package ag
   :ensure t)
 
-(use-package projectile
-  :ensure t
-  :init
-                                        ;(setq projectile-project-search-path '("~/projects/" "~/work/"))
-  :config
-  ;; I typically use this keymap prefix on macOS
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  ;; On Linux, however, I usually go with another one
-  (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
-  (global-set-key (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1))
+;; (use-package projectile
+;;   :ensure t
+;;   :init
+;;                                         ;(setq projectile-project-search-path '("~/projects/" "~/work/"))
+;;   :config
+;;   ;; I typically use this keymap prefix on macOS
+;;   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;;   ;; On Linux, however, I usually go with another one
+;;   (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+;;   (global-set-key (kbd "C-c p") 'projectile-command-map)
+;;   (projectile-mode +1))
+(use-package project
+  :bind-keymap ("C-c p" . project-prefix-map))
 
 (use-package pt
   :ensure t)
